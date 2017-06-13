89d0f0ab62491bf1f90598eda8e9ac7a6144f803
shreyassrinath <shreyassrinath@gmail.com>
2017-06-13 11:22:19 -0400

    removed unzip command

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,7 +1,6 @@
 sudo apt-get update
 sudo apt-get install git -y
 sudo curl -O https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz
-sudo tar -xvf go1.8.3.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.8.3.linux-amd64.tar.gz
 
 echo "export GOPATH=/home/vagrant/" >> /home/vagrant/.bashrc
